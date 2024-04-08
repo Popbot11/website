73ad20d9cda49673e70181216652a4e5859de628
Popbot <legowoodtrashcan@gmail.com>
2024-04-07 21:58:34 -0400

    when

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-var incr = 0;
+var incr = -3;
+var opacity = -0.4;
+
+
 function toggle() {
     var credits = document.getElementById("credits");
     var counter = document.getElementById("counter");
-    var message = document.getElementById("message")    
-    console.log(incr);
+    var message = document.getElementById("message");
+    var arrow = document.getElementById("arrow");
+
     incr++;
+    opacity+=0.001;
     counter.innerHTML = "‎ " + incr;
+    arrow.style.opacity = opacity;
+
     if (credits.style.display === "none") {
         credits.style.display = "block";
     } else {
         credits.style.display = "none";
     }
 
-    if (incr > 1){
+    if (incr > -1){
         counter.style.display = "block";
     }
 
@@ -22,7 +29,9 @@ function toggle() {
     if (incr === 200) {
         counter.innerHTML = "owo";
     }
-    if (incr === 500) {
-        message.innerHTML = "";
+    if (incr === 350) {
+        message.innerHTML = "go outside";
     }    
+    
+
 }
